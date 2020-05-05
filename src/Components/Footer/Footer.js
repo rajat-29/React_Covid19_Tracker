773b1c7ea8a7a8aa0da0dce0cca9ed1efe6cc34c
Rajat Gupta <rajat295gupta@gmail.com>
2020-05-05 14:33:29 +0530

    footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+     return (
+        <div className="footer-copyright bg-dark text-white py-3">
+            @ Rajat Gupta
+        </div>
+     );
+}
+
+export default Footer;
