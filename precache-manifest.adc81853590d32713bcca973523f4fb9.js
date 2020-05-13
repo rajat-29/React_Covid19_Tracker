dd159786606e17dbd21c997cd86887f1ab6a65ce
Rajat Gupta <rajat295gupta@gmail.com>
2020-05-13 20:57:31 +0530

    Updates

---
+++ precache-manifest.adc81853590d32713bcca973523f4fb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499e4076ae337c4ff94aa9040d2b66cd",
+    "url": "/react-covid19-tracker/index.html"
+  },
+  {
+    "revision": "7b3f6f94305c9cea225e",
+    "url": "/react-covid19-tracker/static/css/main.7b6f7319.chunk.css"
+  },
+  {
+    "revision": "e8c2f897ee0b376a33f7",
+    "url": "/react-covid19-tracker/static/js/2.01732a66.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/react-covid19-tracker/static/js/2.01732a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3f6f94305c9cea225e",
+    "url": "/react-covid19-tracker/static/js/main.ad39e053.chunk.js"
+  },
+  {
+    "revision": "f6c020a1b061412e4ef6",
+    "url": "/react-covid19-tracker/static/js/runtime-main.2da9e631.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
